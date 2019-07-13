3ead417d1f68867432665c6222a47d042d698cdf
ProjectRK <scott_rk@yahoo.com>
2019-07-13 13:33:32 +0800

    created pINIT.py

---
+++ pINIT.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.7
+import os
+import sys
+
+print ("Cisco Configuration Initiator\n")
+
+ifc = input("Number of Interfaces: ")
+
+# DEBUG SECTION
+print("\n===== DEBUG SECTION =====\n")
+print("Number of Interfaces:",ifc)
+
+
