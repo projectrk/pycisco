13aceb1f1950b3204c6b871cdd84a5af39854ba6
ProjectRK <scott_rk@yahoo.com>
2019-07-14 12:27:44 +0800

    created pNAT.py

--- pINIT.py
+++ pINIT.py
@@ -5,9 +5,10 @@ import sys
 print ("Cisco Configuration Initiator\n")
 
 ifc = input("Number of Interfaces: ")
+nu = input("Rack Size [1U]: ")
 
 # DEBUG SECTION
 print("\n===== DEBUG SECTION =====\n")
 print("Number of Interfaces:",ifc)
-
+print("Rack Size: " + nu + "U")
 
