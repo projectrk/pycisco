ea96afabcc7411b0fce7122e812a12ae6320bfe9
ProjectRK <scott_rk@yahoo.com>
2019-07-14 12:28:35 +0800

    Making sure of upstream

---
+++ pNAT.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3.7
+import os
+import sys
+
+# INTERFACE
+ifn = input("Number of Interface: ")
+ru = input("Rack Size: ")
+
+# WAN CONFIGURATION
+wanif = input("WAN Interface: ")
+wanip = input("WAN IP: ")
+wansm = input("WAN MASK: ")
+
+# LAN CONFIGURATION
+lanif
+lanip
+lansm
+
+
+# DEBUG SECTION
+print("DEBUG SECTION")
+print("\nNumber of Interfaces:",ifn)
+print("Race Size:",ru)
+
+print("\nWAN Interface:",wanif)
+print("WAN IP:",wanip)
+print("WAN Mask:",wansm)
+
+print("\nLAN Interface:",lanif)
+print("LAN IP:",lanip)
+print("LAN Mask:",lansm)
